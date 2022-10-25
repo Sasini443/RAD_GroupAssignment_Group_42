c7d36962c0a0c94c4a409d2ef73473bb240e3160
Sasini443 <sasinihasinika@gmail.com>
2022-10-25 20:19:54 +0530

    add group assignment

---
+++ BACKEND/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+
+const PORT = process.env.port || 8070;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const URL = process.env.MONGODB_URL;
+mongoose.connect(URL,{
+
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+const connection = mongoose.connection;
+connection.once("open",() => {
+    console.log("Mongodb connection success!");
+})
+
+studentRouter = require("./routes/students.js");
+app.use("/student",studentRouter);
+
+app.listen(PORT, () =>{
+    console.log(`server is up and running on port number: ${PORT}`)
+})
